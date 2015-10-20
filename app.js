0157439b9eb7ea74f6af029d6c4d7eac5cc6a1ec
yovinyip <1271364792@qq.com>
2015-10-20 09:21:54 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require('express');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
-
 var app = express();
 
 app.get('/',function(req,res){
 	res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 5500);
+app.listen(process.env.PORT || 5000);
